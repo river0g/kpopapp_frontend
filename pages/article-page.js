a03a3187a78121c7b21262f6a7fce46cccae77dc
river0g <yui.0g.0418@gmail.com>
2022-05-17 12:04:22 +0900

    記事の取得数変更

--- pages/article-page.js
+++ pages/article-page.js
@@ -71,10 +71,10 @@ export default function ArticlePage({ staticfilteredArticles: articles }) {
 
 export async function getStaticProps() {
   let staticfilteredArticles = await getAllArticlesData();
-  staticfilteredArticles = staticfilteredArticles.filter((_, idx) => {
-    if (idx >= 30) return false;
-    return true;
-  });
+  // staticfilteredArticles = staticfilteredArticles.filter((_, idx) => {
+  //   if (idx >= 30) return false;
+  //   return true;
+  // });
   return {
     props: {
       staticfilteredArticles,
