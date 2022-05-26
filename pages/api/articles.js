4340736e46466f7e98aa181ef72b6491f874a513
river0g <yui.0g.0418@gmail.com>
2022-05-27 05:25:25 +0900

    remove console

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default async function handler(req, res) {
-  console.log("called ApiRoot");
+  // console.log("called ApiRoot");
   let url = "https://jsonplaceholder.typicode.com/users";
 
   const response = await fetch(url);
